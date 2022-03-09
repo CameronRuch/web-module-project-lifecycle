9d3ad344a0a16ab2ee2d8ee844a3140bfbf7a045
CameronRuch <cameron.ruch@yahoo.com>
2022-03-09 03:55:56 -0600

    added post functionality

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import axios from 'axios'
+import Form from './Form'
+import TodoList from './TodoList'
 
 const URL = 'http://localhost:9000/api/todos'
 
 const initialState = {
   successMessage: '',
-  todos : [
-    {name: '', id: '', completed: false}
-  ]
+  todos : [],
 }
 
 export default class App extends React.Component {
@@ -15,11 +15,7 @@ export default class App extends React.Component {
     super(props);
     this.state = initialState
   }
-    
-  componentDidMount() {
-    this.getTodos()
-  }
-
+  
   getTodos = () => {
     axios.get(URL)
       .then(res => {
@@ -28,16 +24,70 @@ export default class App extends React.Component {
           todos: res.data.data,
           successMessage: res.data.message
         })
+        console.log('Grabbing State', this.state)
       })
       .catch(err => {
         this.setState({ ...this.state, errorMessage: 'Uh Oh' })
       })
   }
+
+  componentDidMount() {
+    this.getTodos()
+  }
+
+
+  handleNew = (name) => {
+    const newTodo = {
+      name: name,
+      completed: false
+    }
+    axios.post(URL, newTodo)
+      .then(res => {
+        this.setState({
+          ...this.state, todos: [...this.state.todos, res.data.data]
+        });
+      })
+      .catch(err => {
+        this.setState({
+         ...this.state, 
+         errorMessage: err.response.data.message,
+       })
+      })
+  }
+
+  handleCompleted = () => {
+    this.setState({
+      ...this.state, todos: this.state.todos.filter(todo => {
+        return  (todo.completed === false)
+      })
+    })
+  }
+
+  handleToggle = (clickedId) => {
+    this.setState({
+      ...this.state, 
+      todos: this.state.todos.map(todo => {
+        if (todo.id === clickedId) {
+          return {
+            ...todo, completed: !todo.completed
+          }
+        } 
+          return todo
+        
+      })
+    })
+  }
     
 
   render() {
+    const { todos } = this.state;
     return (
-      <h1>Todos: </h1>
+      <div>
+        <h1>Todos:</h1>
+        <TodoList handleToggle={this.handleToggle} todos={todos} />
+        <Form handleNew={this.handleNew}/>
+        <button onClick={this.handleCompleted}>Clear Completed</button>
+      </div>
     )
   }
 }
