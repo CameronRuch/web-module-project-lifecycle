565889cae3bb08f1a20ffb559a99b0185d095f8a
CameronRuch <cameron.ruch@yahoo.com>
2022-03-08 22:16:52 -0600

    fetch todos from api and set them to state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
+const initialState = {
+  successMessage: '',
+  todos : [
+    {name: '', id: '', completed: false}
+  ]
+}
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState
+  }
+    
+  componentDidMount() {
+    this.getTodos()
+  }
+
+  getTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          todos: res.data.data,
+          successMessage: res.data.message
+        })
+      })
+      .catch(err => {
+        this.setState({ ...this.state, errorMessage: 'Uh Oh' })
+      })
+  }
+    
+
   render() {
-    return null
+    return (
+      <h1>Todos: </h1>
+    )
   }
 }
